e130c96ef211713b92b88de829e35ca0d8da0abc
wutao <wutao91@jd.com>
2019-08-15 16:24:04 +0800

    修改

--- SpringBootDemo-web/src/main/java/com/demo/base/BaseController.java
+++ SpringBootDemo-web/src/main/java/com/demo/base/BaseController.java
@@ -2,14 +2,6 @@ package com.demo.base;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.context.request.WebRequest;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * @Name：BaseController
