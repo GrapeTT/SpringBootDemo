e204a89f313884a86fe0b0887f30651b721a6fce
wutao <wutao91@jd.com>
2019-08-15 16:58:48 +0800

    修改

--- SpringBootDemo-web/src/main/java/com/demo/exception/CustomException.java
+++ SpringBootDemo-web/src/main/java/com/demo/exception/CustomException.java
@@ -26,7 +26,15 @@ public class CustomException  extends Exception{
         this.code = code;
         this.message = message;
     }
-
+    
+    public String getCode() {
+        return code;
+    }
+    
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
     @Override
     public String getMessage() {
         return message;
